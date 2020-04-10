67e53efa83e93c852e92c4bcf22e6b306b71319d
Marcus Nizza <Marcusanizza@gmail.com>
2020-04-09 19:35:43 -0500

    Checking in

--- api/controllers/Index.js
+++ api/controllers/Index.js
@@ -41,13 +41,16 @@ IndexController.get("/buildSatelliteCollection/:page", (req, res) => {
 	res.json();
 });
 IndexController.get("/buildSatelliteLocationCollection", async (req, res) => {
-	var i = 500;
+	SatelliteLocationModel.collection.drop();
+	
+	var i = 0;
 	var satellites = await SatelliteModel.find().exec();
 	//the API requires 1 second between calls, hence the interval
 	var interval = setInterval(() => {
 		if (i <= satellites.length) {
 			IndexController.loadSatLocation(satellites[i].number);
 		}
+		if(i == satellites.length) console.log("All Satellites Accounted For");
 		i++;
 	}, 3500);
 
@@ -69,12 +72,15 @@ IndexController.loadSatLocation = (number) => {
 		if (resp.error) console.log(resp);
 
 		if (!IndexController.isJSON(resp.raw_body)) {
-			console.log("Error, Satellite Not Found by API");
+			console.log(`Error, Satellite Not Found (norad_id: ${number})`);
 			return;
 		}
 		var location = JSON.parse(resp.raw_body);
 		var satXY = new SatelliteLocationModel(location);
-		satXY.save((err, doc) => {});
+		satXY.save((err, doc) => {
+			if(err) console.log(`Error, Could Not Store Sat Location (norad_id: ${number})`)
+			else console.log(`Success, New Location Stored (norad_id: ${number})`)
+		 });
 	});
 };
 IndexController.isJSON = (str) => {
